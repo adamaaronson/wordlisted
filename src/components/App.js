8e40739419b8108bd48915f703c25e0cbd90c749
Adam Aaronson <adamaaronson@gmail.com>
2022-01-02 16:44:30 -0600

    Redesign header

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,24 @@
-import '../index.css';
+import '../css/App.scss';
 import SearchArea from './SearchArea';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <div className="header-wrapper">
-        <header className="header">
-          <a href="/wordlisted" className="title">
-            <i className="fas fa-file-alt title-logo"></i>
-            <span className="title-text title-1">Word</span>
-            <span className="title-text title-2">listed<sup>™</sup></span>
-          </a>
-          <div className="subtitle">Quick wordlist search tool</div>
-          <div className="subsubtitle">
-            <span className="author">by <a href="https://aaronson.org" rel="noreferrer noopener" target="_blank" className="link-border">Adam Aaronson</a></span>
-            <span className="accent"> • </span>
-            <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank"><i className="fab fa-twitter"></i></a>
-            <span className="accent"> • </span>
-            <a href="https://www.buymeacoffee.com/aaronson" rel="noreferrer noopener" target="_blank" className="link-border">Buy me a coffee!</a>
-          </div>
-        </header>
-        <br/>
-        <div className="sub-header">
-            <i className="fas fa-fire-alt announcement-icon"></i>
-            <span className="announcement">New replace and delete search modes!</span>
-        </div>
+      <div className="content">
+        <Header />
+
+        <SearchArea />
+
+        <footer className="footer-wrapper">
+          <p className="copyright-line">
+            © {new Date().getFullYear()} Adam Aaronson • <a href="https://aaronson.org" rel="noreferrer noopener" target="_blank">Aaronson.org</a> • <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank"><i className="fab fa-twitter"></i></a> • <a href="https://github.com/adamaaronson/wordlisted" rel="noreferrer noopener" target="_blank"><i className="fab fa-github"></i></a>
+          </p>
+          <p>
+            Feedback or suggestions? Feel free to <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank" className="link-border">message</a> or <a href="mailto:adamaaronson@gmail.com" className="link-border">email</a> me!
+          </p>
+        </footer>
       </div>
-
-      <SearchArea />
-
-      <footer className="footer-wrapper">
-        <p className="copyright-line">
-          © {new Date().getFullYear()} Adam Aaronson • <a href="https://aaronson.org" rel="noreferrer noopener" target="_blank">Aaronson.org</a> • <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank"><i className="fab fa-twitter"></i></a> • <a href="https://github.com/adamaaronson/wordlisted" rel="noreferrer noopener" target="_blank"><i className="fab fa-github"></i></a>
-        </p>
-        <p>
-          Feedback or suggestions? Feel free to <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank" className="link-border">message</a> or <a href="mailto:adamaaronson@gmail.com" className="link-border">email</a> me!
-        </p>
-      </footer>
-      
     </div>
   );
 }
