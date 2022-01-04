91923a3c0bab9529020415d410165ae8905c6b3d
Adam Aaronson <adamaaronson@gmail.com>
2022-01-04 01:15:13 -0600

    Redesign modal and change a few other things

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../css/InfoModal.scss'
 
 export default class InfoModal extends Component {
     constructor(props) {
@@ -10,31 +11,29 @@ export default class InfoModal extends Component {
 
     render() {
         return (
-            this.props.visible && (
-                <div className="modal-wrapper test-wrapper modal-closer" value="modal-closer" onClick={this.props.onCloseModal}>
-                    <div className="modal">
-                        <div className="modal-text">
-                            <h2 className="modal-header">
-                                <i className="fas fa-info-circle info-icon"></i> About uploading
-                            </h2>
-                            <p>
-                                A wordlist is simply a <span className="accent">.txt</span> or <span className="accent">.dict</span> file with one word on each line.
-                                Any spaces, numbers, or punctuation will be filtered out automatically, so there's no need to remove them yourself.
-                            </p>
-                            <p>
-                                Wordlists can now be <span className="accent">scored</span>! If a word in a list is followed by a semicolon and a number, it will be assigned that score; e.g. <code>AWESOME;100</code> will be scored at 100.
-                                If a word has different scores in multiple lists, the maximum score will apply.
-                            </p>
-                            <p>
-                                This app uses the JavaScript File API and doesn't save your wordlist anywhere, so rest assured that no one can steal your words!
-                            </p>
-                        </div>
-                        <button className="modal-close-button normal-button modal-closer" value="modal-closer" onClick={this.props.onCloseModal}>
-                            Cool beans!
-                        </button>
+            <div className={`modal-wrapper test-wrapper modal-closer ${this.props.visible ? "" : " hidden"}`} onClick={this.props.onCloseModal}>
+                <div className="modal content-box">
+                    <div className="modal-text">
+                        <h1 className="modal-header">
+                            <i className="fas fa-info-circle info-icon"></i> About uploading
+                        </h1>
+                        <p>
+                            A wordlist is simply a <strong>.txt</strong> or <strong>.dict</strong> file with one word on each line.
+                            Any spaces, numbers, or punctuation will be filtered out automatically, so there's no need to remove them yourself.
+                        </p>
+                        <p>
+                            Wordlists can also be scored! If a word in a list is followed by a semicolon and a number, it will be assigned that score; e.g. <code>AWESOME;100</code> will be scored at 100.
+                            If a word has different scores in multiple lists, the maximum score will apply.
+                        </p>
+                        <p>
+                            This app uses the JavaScript File API and doesn't save your wordlist anywhere, so rest assured that no one can steal your words!
+                        </p>
                     </div>
+                    <button className="modal-close-button nice-button small-button modal-closer" onClick={this.props.onCloseModal}>
+                        Cool beans!
+                    </button>
                 </div>
-            )
+            </div>
         )
     }
 }
